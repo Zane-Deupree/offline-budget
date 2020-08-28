063ffa1370cd049a7db283bb4638c137c6c49ee0
Zane Deupree <zdtexan@gmail.com>
2020-08-28 13:55:24 -0500

    new port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 1234;
+const PORT = process.env.PORT || 1234;
 
 const app = express();
 
